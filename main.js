e7ffbbdbcd8fa5f6e2599ac9e3a611ebdfbccf23
xizzana <xizanishvili.99@gmail.com>
2021-12-22 18:17:59 +0400

    initial code

---
+++ main.js
@@ -0,0 +1,23 @@
+let text = document.getElementById('txt');
+let btn = document.getElementById('btn');
+let div = document.getElementById('cardd');
+
+
+let id = 0;
+btn.addEventListener('click', ()=>{
+    id++;
+  if(!text.value == ""){
+     div.innerHTML += `
+         <div class="card mb-1" id="${id}">
+            <div class="card-body d-flex justify-content-between">
+                <span class="card-title">${text.value}</span>
+                <button class="btn btn-danger" onclick="delDiv(${id})" >წაშლა</button>
+            </div>
+         </div>
+     `;
+  }else text.placeholder = "შეიყვანეთ ვის რა უნდა არგოთ !";
+})
+
+function delDiv(id){
+    document.getElementById(`${id}`).style.display = "none";
+}
