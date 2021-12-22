49cfe7d5a698d26315011e0568fe7cfa3ff3aaeb
xizzana <xizanishvili.99@gmail.com>
2021-12-22 19:10:48 +0400

    add with enter

--- main.js
+++ main.js
@@ -15,7 +15,26 @@ btn.addEventListener('click', ()=>{
             </div>
          </div>
      `;
+     text.value = "";
   }else text.placeholder = "შეიყვანეთ ვის რა უნდა არგოთ !";
+})
+
+window.addEventListener("keypress", (e)=>{
+  if(e.keyCode == 13){
+      e.preventDefault();
+    id++;
+    if(!text.value == ""){
+       div.innerHTML += `
+           <div class="card mb-1" id="${id}">
+              <div class="card-body d-flex justify-content-between">
+                  <span class="card-title">${text.value}</span>
+                  <button class="btn btn-danger" onclick="delDiv(${id})" >წაშლა</button>
+              </div>
+           </div>
+       `;
+       text.value = "";
+    }else text.placeholder = "შეიყვანეთ ვის რა უნდა არგოთ !";
+  }
 })
 
 function delDiv(id){
